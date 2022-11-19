7efc2a51678b99063aa00d3d80dc21d485527408
rk <rebeccakruk@outlook.com>
2022-11-19 16:37:01 -0700

    we're still not done

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
+// this will make the Pokemon selected appear on the battle page
 function pokemonSelector(event){
     let parent = event.target.parentElement;
     if(parent.id == `pkmChoice1`){
         let pkmJson = JSON.stringify(pkm1);
-        Cookies.set(`pkmSelection`, pkmJson)
+        Cookies.set(`pkmChoice`, pkmJson)
     } else if (parent.id == `pkmChoice2`){
         let pkmJson = JSON.stringify(pkm2);
-        Cookies.set(`pkmSelection`, pkmJson)
+        Cookies.set(`pkmChoice`, pkmJson)
     } else if (parent.id == `pkmChoice3`){
         let pkmJson = JSON.stringify(pkm3);
-        Cookies.set(`pkmSelection`, pkmJson)
+        Cookies.set(`pkmChoice`, pkmJson)
     }
 }
 
@@ -29,19 +30,23 @@ let pkm1 = {
     attack : [
         {
             name : "water",
-            health : battleNumbers (5, 10)
+            minDamage: 0,
+            maxDamage: 100
         },
         {
             name : "spray",
-            health : battleNumbers (10, 20)
+            minDamage: 0,
+            maxDamage: 100
         },
         {
             name : "punch",
-            health : battleNumbers (10, 15)
+            minDamage: 0,
+            maxDamage: 100
         },
         {
             name : "tackle",
-            health : battleNumbers (10, 20)
+            minDamage: 0,
+            maxDamage: 100
         },
     ]
 }
@@ -51,20 +56,24 @@ let pkm2 = {
     imageUrl: "https://static.pokemonpets.com/images/monsters-images-800-800/155-Cyndaquil.webp",
     attack : [
         {
-            name : "fire",
-            health : battleNumbers (5, 10)
+            name: "fire",
+            minDamage: 0,
+            maxDamage: 100
         },
         {
             name : "roast",
-            health : battleNumbers (10, 20)
+            minDamage: 0,
+            maxDamage: 100
         },
         {
             name : "punch",
-            health : battleNumbers (10, 15)
+            minDamage: 0,
+            maxDamage: 100
         },
         {
             name : "tackle",
-            health : battleNumbers (10, 20)
+            minDamage: 0,
+            maxDamage: 100
         },
     ]
 }
@@ -74,54 +83,54 @@ let pkm3 = {
     imageUrl : `https://assets.pokemon.com/assets/cms2/img/pokedex/full/152.png`,
     attack : [
         {
-            name : "Grass Blade",
-            health : battleNumbers (5, 10)
+            name: "Vine Slap",
+            minDamage: 0,
+            maxDamage: 100
         },
         {
-            name : "Vine Whip",
-            health : battleNumbers (10, 20)
+            name : "Kick",
+            minDamage: 0,
+            maxDamage: 100
         },
         {
-            name : "punch",
-            health : battleNumbers (10, 15)
+            name : "Throat Punch",
+            minDamage: 0,
+            maxDamage: 100
         },
         {
-            name : "tackle",
-            health : battleNumbers (10, 20)
+            name : "Wrestle",
+            minDamage: 0,
+            maxDamage: 100
         },
     ]
 }
 
-
-
 let pokemon1 = document.getElementById(`pkmChoice1`);
-pokemon1.insertAdjacentHTML(`afterbegin`, `<img src="${pkm1.imageUrl}" alt="Todotile">`);
-pokemon1.insertAdjacentHTML(`afterbegin`, `<p>Name: ${pkm1.firstName}</p>`);
 pokemon1.insertAdjacentHTML(`afterbegin`, `<p>Health: ${pkm1.healthPoints}</p>`);
+pokemon1.insertAdjacentHTML(`afterbegin`, `<p>Name: ${pkm1.firstName}</p>`);
+pokemon1.insertAdjacentHTML(`afterbegin`, `<img src="${pkm1.imageUrl}" alt="Todotile">`);
 
 
+// pokemon1.insertAdjacentHTML(`afterbegins`, `<button>${pkm1.attack.name[]}</button>`);
+
+console.log(pkm2.attack[0]); 
+
+
+let pkm1Json = JSON.stringify(pkm1);
+Cookies.set(`pkmChoice`, pkm1Json)
 
 let pokemon2 = document.getElementById(`pkmChoice2`);
-pokemon2.insertAdjacentHTML(`afterbegin`, `<img src="${pkm2.imageUrl}" alt="Cyndaquil">`);
-pokemon2.insertAdjacentHTML(`afterbegin`, `<p>Name: ${pkm2.firstName}</p>`);
 pokemon2.insertAdjacentHTML(`afterbegin`, `<p>Health: ${pkm2.healthPoints}</p>`);
+pokemon2.insertAdjacentHTML(`afterbegin`, `<p>Name: ${pkm2.firstName}</p>`);
+pokemon2.insertAdjacentHTML(`afterbegin`, `<img src="${pkm2.imageUrl}" alt="Cyndaquil">`);
 
 
 let pokemon3 = document.getElementById(`pkmChoice3`);
-pokemon3.insertAdjacentHTML(`afterbegin`, `<img src="${pkm3.imageUrl}" alt="Chikorita">`);
-pokemon3.insertAdjacentHTML(`afterbegin`, `<p>Name: ${pkm3.firstName}</p>`);
 pokemon3.insertAdjacentHTML(`afterbegin`, `<p>Health: ${pkm3.healthPoints}</p>`);
+pokemon3.insertAdjacentHTML(`afterbegin`, `<p>Name: ${pkm3.firstName}</p>`);
+pokemon3.insertAdjacentHTML(`afterbegin`, `<img src="${pkm3.imageUrl}" alt="Chikorita">`);
 
 let selection = []
 pokemon1.querySelector(`a`).addEventListener(`click`, pokemonSelector);
 pokemon2.querySelector(`a`).addEventListener(`click`, pokemonSelector);
 pokemon3.querySelector(`a`).addEventListener(`click`, pokemonSelector);
-
-// let Pkm1Json = JSON.stringify(pkm1);
-// Cookies.set(`pkm1`, enemyJson);
-
-// let Pkm2Json = JSON.stringify(pkm2);
-// Cookies.set(`pkm2`, enemyJson);
-
-// let Pkm3Json = JSON.stringify(pkm3);
-// Cookies.set(`pkm3`, enemyJson);
--- js/battle.js
+++ js/battle.js
@@ -1,67 +1,98 @@
-let selectionJson = Cookies.get(`pkmSelection`);
-let selection = JSON.parse(selectionJson);
-
-document.body.insertAdjacentHTML(`afterend`, `<img src="${selection.imageUrl}">`);
-document.body.insertAdjacentHTML(`afterend`, `<p>Name: ${selection.firstName}</p>`);
-document.body.insertAdjacentHTML(`afterend`, `<p>Health:${selection.healthPoints}</p>`);
-// document.body.insertAdjacentHTML(`afterbegin`, `<p>Attack:${selection.attack}</p>`);
-/** Variable names placeholders
-attackName = ``;
-Curattack{};
-randomInt =
-randomInt =
-enemyAttack = 
-characters = [],
-*/
-
 function battleNumbers (max, min){
     // generate a random number
     if(min === undefined || min === '' || min === null){
         min = 0;
     }
     return Math.floor(Math.random() * (max - min) + min);
+
 }
 
-// TEMPORARY CALL
-// document.body.getElementById(`kick`).addEventListener(`click`, battleNumbers);
+let selectionJson = Cookies.get(`pkmChoice`);
+let selection = JSON.parse(selectionJson);
 
-function attackHandler(event){
-    let parent = event.target.parentElement;
-    if(parent.id =="attack"){
-    let attackJson = JSON.stringify(attack)
-    Cookies.set(`attackSelection`, attackJson)
-    }
+
+function makeAttack(event){
+    let damage = battleNumbers(selection.attack[0].maxDamage, selection.attack[0].minDamage);
+    let hit = (enemy.health - damage);
+    console.log(hit);
+//     Cookies.set(enemyHealth);
+//     if(enemyHealth - damage)
+//     damage = (damage - enemy.health)
+//  console.log(damage);
 }
 
-function playerHealth(){
-    
+//Attempts to set timeouts for enemy attack
+function makeEnemyAttack(){
+    let enemyDamage = battleNumbers(enemy.attack[0].maxDamage, selection.attack[0].minDamage);
+    enemyDamage - playerHealth
+} 
+//trying to make it so the enemy attacks after one of the attack buttons in clicked
+function enemyAttack(){
+    document.getElementById(`attackButtons`)
+} 
+//Attempting to make it so that our buttons are locked until enemy attack
+var attackdelay = document.getElementById(`attackButtons`)
+attackdelay.onclick = function() {
+    attackdelay.disabled = true;
+    setTimeout(function() {
+        attackdelay.disabled = false;
+    }, 2000);
 }
 
 
 let enemy = {
     firstName : `Mark`,
     health : 100,
+    // imageUrl: ""
     attack : [
         {
-            name : "slap",
-            health : battleNumbers (5, 10)
+            name : "Teach",
+            minDamage: 0,
+            maxDamage: 100
         },
         {
-            name : "kick",
-            health : battleNumbers (10, 20)
+            name : "Code",
+            minDamage: 0,
+            maxDamage: 100
         },
         {
-            name : "punch",
-            health : battleNumbers (10, 15)
+            name : "Tangent",
+            minDamage: 0,
+            maxDamage: 100
         },
         {
-            name : "tackle",
-            health : battleNumbers (10, 20)
+            name : "Text Message",
+            minDamage: 0,
+            maxDamage: 100
         },
     ],
-    imageUrl : `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRah_GffKal78XJXMGogQ9k2m-crNep-S6lsw&usqp=CAU`
+    // imageUrl : `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRah_GffKal78XJXMGogQ9k2m-crNep-S6lsw&usqp=CAU`
 }
 
+let playerHealth = selection.healthPoints;
 
-let enemyJson = JSON.stringify(enemy);
-cookies.set(`enemy`, enemyJson)
+document.getElementById(`attack1`).addEventListener(`click`, makeAttack);
+document.getElementById(`attack2`).addEventListener(`click`, makeAttack);
+document.getElementById(`attack3`).addEventListener(`click`, makeAttack);
+document.getElementById(`attack4`).addEventListener(`click`, makeAttack);
+document.getElementById(`attackButtons`).addEventListener(`click`, enemyAttack);
+
+document.body.insertAdjacentHTML(`afterend`, `<img src="${selection.imageUrl}">`);
+document.body.insertAdjacentHTML(`afterend`, `<p>${selection.firstName}</p>`);
+document.body.insertAdjacentHTML(`afterend`, `<p>Health:${selection.healthPoints}</p>`);
+document.body.insertAdjacentHTML(`afterbegin`, `<button id="attack1">${selection.attack[0].name}</button>`);
+document.body.insertAdjacentHTML(`afterbegin`, `<button id="attack2">${selection.attack[1].name}</button>`);
+document.body.insertAdjacentHTML(`afterbegin`, `<button id="attack3">${selection.attack[2].name}</button>`);
+document.body.insertAdjacentHTML(`afterbegin`, `<button id="attack4">${selection.attack[3].name}</button>`);
+document.body.insertAdjacentHTML(`afterend`, `<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1PAZwO-jR5Z842vckeoRajOPPWhq38asBoA&usqp=CAU"`);
+// enemy.insertAdjacentHTML(`afterend`,`<p>${[enemy][health]}</p>`);
+
+console.log(selection.attack[0].minDamage);
+console.log(selection.attack[0].maxDamage);
+
+
+
+let computerCurrentHealth = enemy.health;
+console.log(computerCurrentHealth);
+
+
